8cefb8267e472102acb2490a11ee2842a9185b11
Aparichit0 <0.dharmveersingh@gmail.com>
2022-03-20 22:41:31 +0530

    take userInput

---
+++ script.js
@@ -0,0 +1,4 @@
+let userInput = prompt("please enter your move: ", "rock");
+let userSelection = userInput.toLowerCase();
+const gameMoves = ["rock", "paper", "scissors"];
+    console.log("you have selected: " + userSelection);
