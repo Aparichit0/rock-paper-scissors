5a4997af94f6823f4703af58fe0b3139d86c6b24
Aparichit0 <0.dharmveersingh@gmail.com>
2022-03-22 18:08:35 +0530

    console gamePlay() is ready!

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-let userInput = prompt("please enter your move: ", "rock");
-let userSelection = userInput.toLowerCase();
 const gameMoves = ["rock", "paper", "scissors"];
-    console.log("you have selected: " + userSelection);
+
+let userInput = prompt("please enter your move: ", "Scissors");
+let userSelection = userInput.toLowerCase();
+//random move selection by computer
+let randomNumber = Math.floor(Math.random()*3);
+const computerSelection = gameMoves[randomNumber];
+
+const gameplay = () => {
+    const indexDiff = gameMoves.indexOf(userSelection) - gameMoves.indexOf(computerSelection);
+    switch (indexDiff) {
+        case 0 :
+            return `It's a Tie!\nboth parties choose ${computerSelection}.`;
+            break;
+        case 1:
+        case -2:
+            return `You Won!\n${userSelection} beats ${computerSelection}.`;
+            break;
+        case -1:
+        case 2:
+            return `You Lose!\n${computerSelection} beats ${userSelection}.`;
+            break;
+        default:
+            return "uh ohh... something went wrong!";
+    };
+};
+
+console.log(gameplay());
+
