083ec7654db3d0548691ca93228de5fb01c2b45c
Aparichit0 <0.dharmveersingh@gmail.com>
2022-03-25 16:53:33 +0530

    added "best of five" mode
    whoever scores more during 5 rounds is the winner!
    this functionality is handled by "game()"

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function computerPlay() {
     return gameMoves[randomNumber];
 };
 
+let computerScore = 0;
+let userScore = 0;
+
 // Play one round of rock paper scissors game
 function playRound(userSelection = userInput, computerSelection = computerPlay()) {
     const indexDiff = gameMoves.indexOf(userSelection) - gameMoves.indexOf(computerSelection);
@@ -19,10 +22,12 @@ function playRound(userSelection = userInput, computerSelection = computerPlay()
             break;
         case 1:
         case -2:
+            userScore++;
             return `You Won!\n${userSelection} beats ${computerSelection}.`;
             break;
         case -1:
         case 2:
+            computerScore++;
             return `You Lose!\n${computerSelection} beats ${userSelection}.`;
             break;
         default:
@@ -30,5 +35,17 @@ function playRound(userSelection = userInput, computerSelection = computerPlay()
     };
 };
 
-console.log(playRound());
+function game(){
+   for (i = 0; i < 5; i++) {
+        console.log(playRound());
+    };
+    if (userScore > computerScore){
+        return `You Won!\nuser score: ${userScore}\ncomputer score: ${computerScore}`;
+    } else if (userScore == computerScore){
+        return `It's an ultimate Tie!!!\nuser score: ${userScore}\ncomputer score: ${computerScore}`;
+    } else {
+        return `You Lose!\nuser score: ${userScore}\ncomputer score: ${computerScore}`;
+    };
+};
 
+console.log(game());
