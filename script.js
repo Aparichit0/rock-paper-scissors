7decc13bb406371f83582375f836a02cc2756d21
aparichit0 <0.dharmveersingh@gmail.com>
2022-06-01 20:44:18 +0530

    replace userInput taking method from prompt to button click
    and some code formation along the way

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
 const gameMoves = ["rock", "paper", "scissors"];
+const gameMoveBtns = document.querySelectorAll(".gameMove");
 
-//taking User input in case insensitive
-let userInput = () => prompt("please enter your move: ", "Scissors").toLowerCase();
+//user clicked a button
+gameMoveBtns.forEach((btn) => {
+  btn.addEventListener("click", userClick);
+});
 
-//random move for computer
-function computerPlay() {
-    let randomNumber = Math.floor(Math.random()*3);
-    return gameMoves[randomNumber];
-};
-
+let userInput = "";
 let computerScore = 0;
 let userScore = 0;
 
+// Handling User input & playing a single round
+function userClick() {
+  userInput = this.dataset.move;
+  console.log(playRound());
+}
+
+// Random move generation for computer
+function computerPlay() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  return gameMoves[randomNumber];
+}
+
 // Play one round of rock paper scissors game
-function playRound(userSelection = userInput(), computerSelection = computerPlay()) {
-    // determine results based on selected move's position (index value) within "gameMoves" array
-    const indexDiff = gameMoves.indexOf(userSelection) - gameMoves.indexOf(computerSelection);
-    switch (indexDiff) {
-        case 0 :
-            return `It's a Tie!\nboth parties choose ${computerSelection}.`;
-            break;
-        case 1:
-        case -2:
-            userScore++;
-            return `You Won!\n${userSelection} beats ${computerSelection}.`;
-            break;
-        case -1:
-        case 2:
-            computerScore++;
-            return `You Lose!\n${computerSelection} beats ${userSelection}.`;
-            break;
-        default:
-            return "uh ohh... something went wrong!";
-    };
-};
+function playRound(
+  userSelection = userInput,
+  computerSelection = computerPlay()
+) {
+  // determine results based on selected move's position (index value) within "gameMoves" array
+  const indexDiff =
+    gameMoves.indexOf(userSelection) - gameMoves.indexOf(computerSelection);
+  switch (indexDiff) {
+    case 0:
+      return `It's a Tie!\nboth parties choose ${computerSelection}.`;
+      break;
+    case 1:
+    case -2:
+      userScore++;
+      return `You Won!\n${userSelection} beats ${computerSelection}.`;
+      break;
+    case -1:
+    case 2:
+      computerScore++;
+      return `You Lose!\n${computerSelection} beats ${userSelection}.`;
+      break;
+    default:
+      return "uh ohh... something went wrong!";
+  }
+}
 
 // function game(){
 //    for (i = 0; i < 5; i++) {
